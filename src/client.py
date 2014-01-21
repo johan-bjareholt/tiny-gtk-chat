8688228f1627461bed83ab23f2e3235fa82321df
johan-bjareholt <johanthepro@gmail.com>
2014-01-21 11:17:11 +0100

    Client and serverside improvements - Enter now works in chatbox - motd added - connection, disconnection and socket error handling improved

--- src/client.py
+++ src/client.py
@@ -17,6 +17,7 @@ class ConnectDialog(Gtk.Dialog):
         box.add(label)
 
         self.entry = Gtk.Entry()
+        #self.entry.connect("activate", Gtk.ResponseType.OK)
         box.add(self.entry)
 
         self.show_all()
@@ -28,23 +29,24 @@ class MainWindow(Gtk.Window):
         self.set_border_width(5)
         self.set_default_size(400, 200)
 
-        grid = Gtk.Grid(column_homogeneous=True, column_spacing=10, row_spacing=10)
-        self.add(grid)
+        self.grid = Gtk.Grid(column_homogeneous=True, column_spacing=10, row_spacing=10)
+        self.add(self.grid)
 
         # Connect button
-        self.connectbutton = Gtk.Button()
-        icon = Gio.ThemedIcon(name="gtk-network")
-        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
-        label = Gtk.Label("Connect...")
-        connectbox = Gtk.Box(spacing=5)
-        connectbox.pack_start(image, True, True, 0)
-        connectbox.pack_start(label, True, True, 0)
-        self.connectbutton.add(connectbox)
+        self.connectbutton = self.make_header_icon_button('Connect...', 'gtk-connect')
         self.connectbutton.connect("clicked", self.connect_dialog)
+        self.favoritesbutton = self.make_header_icon_button('Favorites', 'gtk-directory')
+        self.favoriteaddbutton = self.make_header_icon_button('Add to favorites', 'gtk-add')
 
-        headericonbox = Gtk.Box(spacing=5)
-        headericonbox.add(self.connectbutton)
-        grid.attach(headericonbox, 0, 0, 1, 1)
+
+        # Create header icon box
+        self.headericonbox = Gtk.Box(spacing=5)
+        self.grid.attach(self.headericonbox, 0, 0, 2, 1)
+
+        # Add buttons to header icon box
+        self.headericonbox.add(self.connectbutton)
+        #self.headericonbox.add(self.favoritesbutton)
+        #self.headericonbox.add(self.favoriteaddbutton)
 
         # Scrollable chat window
         self.chatwindow = Gtk.ScrolledWindow()
@@ -55,12 +57,13 @@ class MainWindow(Gtk.Window):
         self.chatbox.set_editable(False)
         self.chatbox.set_cursor_visible(False)
         self.chatwindow.add(self.chatbox)
-        grid.attach(self.chatwindow, 0, 1, 2, 1)
+        self.grid.attach(self.chatwindow, 0, 1, 2, 1)
 
 
         # Message input
         self.chatinputentry = Gtk.Entry()
         self.chatinputentry.set_text("Hello World")
+        self.chatinputentry.connect("activate", self.on_button_clicked)
         # Send message button
         self.messagesendbutton = Gtk.Button(label="Send message")
         self.messagesendbutton.connect("clicked", self.on_button_clicked)
@@ -68,9 +71,20 @@ class MainWindow(Gtk.Window):
         messagebox = Gtk.Box(spacing=5)
         messagebox.pack_start(self.chatinputentry, True, True, 0)
         messagebox.pack_end(self.messagesendbutton, False, False, 0)
-        grid.attach(messagebox, 0, 3, 2, 1)
+        self.grid.attach(messagebox, 0, 3, 2, 1)
 
         self.addChatEntry("Press the connect button and specify ip and port to connect")
+
+    def make_header_icon_button(self, label, icon):
+        iconbutton = Gtk.Button()
+        icon = Gio.ThemedIcon(name=icon)
+        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
+        label = Gtk.Label(label)
+        buttonbox = Gtk.Box(spacing=5)
+        buttonbox.pack_start(image, True, True, 0)
+        buttonbox.pack_start(label, True, True, 0)
+        iconbutton.add(buttonbox)
+        return iconbutton
 
     def connect_dialog(self, widget):
         dialog = ConnectDialog(self)
@@ -112,15 +126,25 @@ class Connection(threading.Thread):
         try:
             self.socket.connect((self.address, self.port))
         except Exception as e:
-            print(e)
+            win.addChatEntry(e)
+            self.disconnect
         win.addChatEntry("Connected!")
         self.connected = True
 
-        while self.connected:
-            data = self.socket.recv(2048)
-            if data:
-                win.addChatEntry(data)
+        try:
+            while self.connected:
+                    data = self.socket.recv(2048)
+                    if data:
+                        win.addChatEntry(data)
+        except Exception as e:
+            win.addChatEntry(e)
+        self.disconnect()
         self.socket.close()
+
+    def disconnect(self):
+        self.connected = False
+        win.update_connect_icon()
+        self.stop()
 
     def sendData(self, data):
         if self.connected:
@@ -130,10 +154,9 @@ class Connection(threading.Thread):
 
 if __name__ == '__main__':
     GObject.threads_init()
+    connection = Connection()
     win = MainWindow()
     win.connect("delete-event", Gtk.main_quit)
     win.show_all()
-
-    connection = Connection()
 
     Gtk.main()
--- src/server.py
+++ src/server.py
@@ -9,6 +9,10 @@ server_address = '127.0.0.1'
 port = 1337
 
 
+motd = ("Welcome to Johans tiny-gtk-chat server!\n"+
+	   "glhf guise")
+
+
 class Client(threading.Thread):
 	def __init__(self, connection, address):
 		threading.Thread.__init__(self)
@@ -16,25 +20,35 @@ class Client(threading.Thread):
 		self.address = address
 		self.connected = True
 		self.daemon = True
-		self.name = "noname"
+		self.name = self.address
+		self.username = "noname"
 		self.subscriptions = []
 		self.start()
 
 	def run(self):
 		print('Client {} Connected!'.format(self.address))
 		connected_clients.append(self)
+		self.send_data(motd)
 		chatroom.subscribe(self)
 
-		while self.connected:
-			data = self.connection.recv(2048)
-			if data[0] == '/':
-				command_handler(self, data, chatroom)
-			else:
-				chatroom.new_message(self, data)
+		try:
+			while self.connected:
+				data = self.connection.recv(2048)
+				if data:
+					if data[0] == '/':
+						command_handler(self, data, chatroom)
+					else:
+						chatroom.new_message(self.username, data)
+		except Exception as e:
+			print(str(self.address) + " encountered an error!\n" + str(e))
+
+		self.disconnect()
+
+	def disconnect(self):
 		self.connection.close()
 
 		for channel in self.subscriptions:
-			channel.connected.remove(self)
+			channel.unsubscribe(self)
 
 		connected_clients.remove(self)
 
@@ -48,32 +62,37 @@ class ChatRoom():
 		self.connected = []
 
 	def new_message(self, user, message):
-		data = user.name + ": " + message
+		data = user + ": " + message
 		for user in self.connected:
 			try:
 				user.send_data(data)
 			except socket.error as e:
-				#if e.errno == 32: # Broken Pipe
-				#	print(user + " is no longer connected")
-				#	user.connected = False
-				#else:
-				print("Socket error:" + e) 
+				print("Socket error:" + str(e)) 
 			except Exception as e:
 				print(e)
 
 	def subscribe(self, user):
 		self.connected.append(user)
 		user.subscriptions.append(self)
+		self.new_message("Info", user.username + " joined the chatroom")
+
+	def unsubscribe(self, user):
+		self.connected.remove(user)
+		user.subscriptions.remove(self)
+		self.new_message("Info", user.username + " left the chatroom")
 
 def command_handler(user, message, channel):
 	message_parameters = message.split(" ")
 	if message_parameters[0] == "/username":
-		user.name = message_parameters[1]
+		user.username = message_parameters[1]
 		user.send_data("Your name was successfully changed to " + message_parameters[1])
+	elif message_parameters[0] == "/motd":
+		user.send_data(motd)
 	elif message_parameters[0] == "/help":
 		user.send_data("Available commands: \n"+
-					   "/username [username] - Changes your username\n"+
-					   "/help - shows this help message\n")
+					   "/username [username] - changes your username\n"+
+					   "/help - shows this help message\n"+
+					   "/motd - shows the message of the day\n")
 	else:
 		user.send_data("Unknown command!\nType /help for available commands")
 
